236eceaba8c4509efe855d3a69aed5139c571831
JUNSHIROBABA <59195753+JUNSHIROBABA@users.noreply.github.com>
2020-07-14 08:05:23 +0900

    preview

--- app/assets/javascript/form.js
+++ app/assets/javascript/form.js
@@ -1,9 +1,22 @@
 $(function() {
-  // disable auto discover
-  Dropzone.autoDiscover = false;
+  $('.file_select').on('change',function(e){
+    var files = e.target.files;
+    var d = (new $.Deferred()).resolve();
+    $.each(files,function(i,file){
+      d = d.then(function(){return previewImage(file)});
+    });
+  })
 
-  $(".dropzone").dropzone({
-    maxFilesize: 1,
-    addRemoveLinks: true
-  });
+  var previewImage = function(imageFile){
+    var reader = new FileReader();
+    var img = new Image();
+    var def =$.Deferred();
+    reader.onload = function(e){
+      $('.preview').append(img);
+      img.src = e.target.result;
+      def.resolve(img);
+    };
+    reader.readAsDataURL(imageFile);
+    return def.promise();
+  }
 });
