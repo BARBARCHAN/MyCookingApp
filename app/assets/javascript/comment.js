f3fb4cca556c16bfa6886905156c020087e1d7dd
JUNSHIROBABA <59195753+JUNSHIROBABA@users.noreply.github.com>
2020-08-04 11:01:29 +0900

    コメントテンプレートリテラルの編集

--- app/assets/javascript/comment.js
+++ app/assets/javascript/comment.js
@@ -1,5 +1,23 @@
 $(function() {
 
+  function buildHTML(comment){
+    var html = `
+      <div class="comment-upper">
+        <div class="comment-user">
+          馬場惇史郎
+        </div>
+        <div class="comment-date">
+          2020年08月04日 01時44分
+        </div>
+       </div>
+       <div class="comment-lower">
+          <div class="comment-text">
+            aaa
+          </div>
+        </div>`
+  
+  }
+
   $('#post_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -12,6 +30,15 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comment-area').append(html);
+      $('.textbox').val('');
+      $('.comment-send-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('コメントの投稿に失敗しました');
     })
   });
 });
